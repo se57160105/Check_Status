8b82d0ec6e04c96394b55da96d13ce568ba648e8
se57160105 <se57160105@gmail.com>
2017-05-09 13:04:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/teerawat/check_status3/Tab2_Payment.java
@@ -0,0 +1,21 @@
+package com.example.teerawat.check_status3;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+/**
+ * Created by Teerawat on 09-May-17.
+ */
+
+public class Tab2_Payment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+        return rootView;
+    }
+}
